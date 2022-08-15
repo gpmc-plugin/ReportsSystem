b8ec1caea23ca37f86ebf651784ec0df1482db4a
pythontest <50592309+mikimasn@users.noreply.github.com>
2022-08-15 18:55:56 +0200

    fix bo idont z szylazem sie zesra

--- src/main/java/me/gpmcplugins/reportssystem/Commands/ReportContinueCommand.java
+++ src/main/java/me/gpmcplugins/reportssystem/Commands/ReportContinueCommand.java
@@ -34,39 +34,7 @@ public class ReportContinueCommand implements CommandExecutor {
                 }
                 switch (reportState){
                     case 0:
-                        switch (args[0]) {
-                            case "0":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Death_Bug);
-                                break;
-                            case "1":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Death_Other_Player);
-                                break;
-                            case "2":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.User_Cheating);
-                                break;
-                            case "3":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.User_Scam);
-                                break;
-                            case "4":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Message_Bad_Words);
-                                break;
-                            case "5":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Message_Scam);
-                                break;
-                            case "6":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Message_Hate_Speach);
-                                break;
-                            case "7":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Message_Offensive);
-                                break;
-                            case "8":
-                                report.setShortDescription(ReportCreator.ReportShortDescription.Other);
-                                break;
-                            default:
-                                sender.sendMessage("Zły typ'");
-                                return false;
-
-                        }
+                        report.setShortDescription(ReportCreator.ReportShortDescription.values()[Integer.parseInt(args[0])]);
                         playerCreationStatus.setState(playerCreationStatus.getState()+1);
                         playerCreationStatus.getPlayer().openInventory(Bukkit.createInventory(null, InventoryType.CHEST));
                         playerCreationStatus.getPlayer().closeInventory();
