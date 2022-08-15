e074073d5f1d259fcd00e74ae721a2d013202d77
Idontpaytowin <74816571+Idontpaytowin@users.noreply.github.com>
2022-08-15 14:11:53 +0200

    added reporttype check

--- src/main/java/me/gpmcplugins/reportssystem/Commands/ReportCommand.java
+++ src/main/java/me/gpmcplugins/reportssystem/Commands/ReportCommand.java
@@ -3,9 +3,41 @@ package me.gpmcplugins.reportssystem.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import me.gpmcplugins.reportssystem.objects.ReportCreator.ReportType;
+
 public final class ReportCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can run this command.");
+            return true;
+        }
+
+        Player p = (Player) sender;
+        ReportType reportType;
+
+        switch(args[0])
+        {
+            case "death":
+                reportType = ReportType.Death;
+                sendNotImplemented(p);
+                break;
+            case "message":
+                reportType = ReportType.Message;
+                sendNotImplemented(p);
+                break;
+            case "user":
+                reportType = ReportType.User;
+                sendNotImplemented(p);
+                break;
+        }
+
+
+        return true;
+    }
+    public void sendNotImplemented(Player p)
+    {
+        p.sendMessage("Not implemented");
     }
 }
