084f7ede46981370f0802aaef71621d13a8bf73e
Idontpaytowin <74816571+Idontpaytowin@users.noreply.github.com>
2022-08-16 13:24:26 +0200

    create report review command

--- src/main/java/me/gpmcplugins/reportssystem/Commands/ReportReviewCommand.java
+++ src/main/java/me/gpmcplugins/reportssystem/Commands/ReportReviewCommand.java
@@ -15,8 +15,50 @@ public class ReportReviewCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
+        switch (args[0])
+        {
+            case "claimnewreportgui":
+                sendNotImplemented(sender);
+                break;
+            case "continueclaimedreportgui":
+                sendNotImplemented(sender);
+                break;
+            case "claim":
+                if (args[1] == null)
+                {
+                    sender.sendMessage("you forgot about report id");
+                    return false;
+                }
+                Integer claimId = Integer.parseInt(args[1]);
+                break;
+            case "accept":
+                if (args[1] == null)
+                {
+                    sender.sendMessage("you forgot about report id");
+                    return false;
+                }
+                sendNotImplemented(sender);
+                Integer acceptId = Integer.parseInt(args[1]);
+                break;
+            case "deny":
+                if (args[1] == null)
+                {
+                    sender.sendMessage("you forgot about report id");
+                    return false;
+                }
+                Integer denyId = Integer.parseInt(args[1]);
+                sendNotImplemented(sender);
+                sendNotImplemented(sender);
+                break;
+            default:
+                sendNotImplemented(sender);
+                break;
+        }
+        return true;
+    }
+    public void sendNotImplemented(CommandSender sender)
+    {
         sender.sendMessage("Not implemented");
-        return false;
     }
 
 }
