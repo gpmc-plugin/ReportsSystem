d2879bb16678e761710acd5ae0606dec736d4891
pythontest <50592309+mikimasn@users.noreply.github.com>
2022-08-16 13:07:05 +0200

    Add report status

--- src/main/java/me/gpmcplugins/reportssystem/objects/ReportObject.java
+++ src/main/java/me/gpmcplugins/reportssystem/objects/ReportObject.java
@@ -3,6 +3,10 @@ package me.gpmcplugins.reportssystem.objects;
 import me.gpmcplugins.reportssystem.reportssystem.ReportsSystem;
 import org.bukkit.entity.Player;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.UUID;
 
@@ -26,5 +30,44 @@ public class ReportObject {
         this.admin = plugin.getServer().getPlayer(UUID.fromString(admin));
         this.timestamp = new Date(timestamp);
 
+    }
+    public ReportStatus getReportStatus(){
+        Connection conn = plugin.getDatabaseManager().getConn();
+        String sql = "Select status from reports_status where id=?";
+        try {
+            PreparedStatement prstm = conn.prepareStatement(sql);
+            prstm.setInt(1,this.id);
+            ResultSet rs=prstm.executeQuery();
+            if(rs.next()){
+                ReportStatus reportStatus = ReportStatus.valueOf(rs.getString("status"));
+                if(reportStatus==null)
+                    return ReportStatus.In_Progress;
+                else
+                    return reportStatus;
+            }
+
+            else
+                return ReportStatus.In_Progress;
+        } catch (SQLException e) {
+            plugin.getDatabaseManager().throwError(e.getMessage());
+        }
+        return ReportStatus.In_Progress;
+    }
+    public void setReportStatus(ReportStatus reportStatus){
+        Connection conn = plugin.getDatabaseManager().getConn();
+        String sql = "UPDATE \"main\".\"reports_status\" SET \"status\"=? WHERE \"id\"=?";
+        try {
+            PreparedStatement prstm = conn.prepareStatement(sql);
+            prstm.setString(1,reportStatus.toString());
+            prstm.setInt(2,this.id);
+        } catch (SQLException e) {
+            plugin.getDatabaseManager().throwError(e.getMessage());
+        }
+
+    }
+    public enum ReportStatus{
+        In_Progress,
+        Accepted,
+        Denided,
     }
 }
